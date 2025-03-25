cdfa7e14547d14da64afefda514c8a3b5bec6efb
Max <maxolchov@gmail.com>
2025-03-25 01:40:45 +0100

    Initial commit

---
+++ src/main/java/net/fts/drugs/plugin/DrugsPlugin.java
@@ -0,0 +1,17 @@
+package net.fts.drugs.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DrugsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
